711cabd5f9a2efdaaedc49049000fb1062f5a219
JoaoP-Souza <joao.paulo.souza@gec.inatel.br>
2023-12-10 18:40:31 -0300

    subindo funcionalidade que valida nota e exibe media delas

--- CinemaList/src/app/app.module.ts
+++ CinemaList/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
 import { AdicionarFilmeComponent } from './adicionar-filme/adicionar-filme.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CinemaList/src/app/lista-filmes/lista-filmes.component.ts
+++ CinemaList/src/app/lista-filmes/lista-filmes.component.ts
@@ -11,6 +11,7 @@ export class ListaFilmesComponent implements OnInit {
   constructor() { }
 
   filmes: Filme[] = [];
+  mediaNotas: number = 0; // Propriedade para armazenar a média
 
   newFilme: Filme = {
     nome: '',
@@ -24,86 +25,99 @@ export class ListaFilmesComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddMovieClick(  title?: string, genre?: string, year?: string, rating?: string, teste?: boolean) {
-    if(teste == true){
-        if (title && genre && year && rating) {
+  onAddMovieClick(title?: string, genre?: string, year?: string, rating?: string, teste?: boolean) {
+    if (teste == true) {
+      if (title && genre && year && rating) {
+        const numericRating = parseFloat(rating);
+  
+        if (!isNaN(numericRating) && numericRating >= 0 && numericRating <= 10) {
           this.newFilme.nome = title;
           this.newFilme.genero = genre;
           this.newFilme.ano = year;
-          this.newFilme.nota = rating;
-
-        this.filmes.push(this.newFilme);
-        this.newFilme = {
-          nome: '',
-          genero: '',
-          ano: '',
-          nota: ''
-        };
-<<<<<<< HEAD
-
+          this.newFilme.nota = numericRating.toString();
+  
+          this.filmes.push(this.newFilme);
+          this.newFilme = {
+            nome: '',
+            genero: '',
+            ano: '',
+            nota: ''
+          };
+  
           return 'Filme adicionado com sucesso!';
-      
         } else {
-=======
-          return 'Filme adicionado com sucesso!';
-
-        } else {
-          
->>>>>>> 94065975a743af9da63cc5bff85195be0ef1c8d8
-          return 'Por favor, preencha todos os campos obrigatórios.';
+          return 'A nota deve ser um número entre 0 e 10.';
         }
-
-  }else{
-
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 94065975a743af9da63cc5bff85195be0ef1c8d8
-    const titleInput = (document.querySelector('#title') as HTMLInputElement);
-    const genreInput = (document.querySelector('#genre') as HTMLInputElement);
-    const yearInput = (document.querySelector('#year') as HTMLInputElement);
-    const ratingInput = (document.querySelector('#rating') as HTMLInputElement);
-
-    if (titleInput && genreInput && yearInput && ratingInput) {
-      this.newFilme.nome = titleInput.value.toString();
-      this.newFilme.genero = genreInput.value;
-      this.newFilme.ano = yearInput.value;
-      this.newFilme.nota = ratingInput.value;
-
-      // Verificar se os campos obrigatórios não estão vazios
-      if (this.newFilme.nome && this.newFilme.genero && this.newFilme.ano && this.newFilme.nota) {
-        this.filmes.push(this.newFilme);
-        this.newFilme = {
-          nome: '',
-          genero: '',
-          ano: '',
-          nota: ''
-        };
-
-        // Limpar os campos após a adição
-        titleInput.value = '';
-        genreInput.value = '';
-        yearInput.value = '';
-        ratingInput.value = '';
-
-        return 'Filme adicionado com sucesso!';
       } else {
         return 'Por favor, preencha todos os campos obrigatórios.';
       }
     } else {
-      return 'Erro ao acessar os campos de entrada.';
-    }}
+      // Your existing code for handling form inputs manually
+      const titleInput = (document.querySelector('#title') as HTMLInputElement);
+      const genreInput = (document.querySelector('#genre') as HTMLInputElement);
+      const yearInput = (document.querySelector('#year') as HTMLInputElement);
+      const ratingInput = (document.querySelector('#rating') as HTMLInputElement);
+  
+      if (titleInput && genreInput && yearInput && ratingInput) {
+        this.newFilme.nome = titleInput.value.toString();
+        this.newFilme.genero = genreInput.value;
+        this.newFilme.ano = yearInput.value;
+  
+        // Ensure the rating is within the valid range
+        const numericRating = parseFloat(ratingInput.value);
+        if (!isNaN(numericRating) && numericRating >= 0 && numericRating <= 10) {
+          this.newFilme.nota = numericRating.toString();
+  
+          // Verificar se os campos obrigatórios não estão vazios
+          if (this.newFilme.nome && this.newFilme.genero && this.newFilme.ano && this.newFilme.nota) {
+            this.filmes.push(this.newFilme);
+            this.newFilme = {
+              nome: '',
+              genero: '',
+              ano: '',
+              nota: ''
+            };
+  
+            // Limpar os campos após a adição
+            titleInput.value = '';
+            genreInput.value = '';
+            yearInput.value = '';
+            ratingInput.value = '';
+  
+            this.calcularMedia();
+            return 'Filme adicionado com sucesso!';
+          } else {
+            return 'Por favor, preencha todos os campos obrigatórios.';
+          }
+        } else {
+          return 'A nota deve ser um número entre 0 e 10.';
+        }
+      } else {
+        return 'Erro ao acessar os campos de entrada.';
+      }
+    }
   }
 
   deleteFilme(filme: Filme) {
     const index = this.filmes.indexOf(filme);
     if (index !== -1 && index < this.filmes.length) {
       this.filmes.splice(index, 1);
+      this.calcularMedia();
       return 'Filme excluído com sucesso!';
     } else {
       return 'Erro ao excluir o filme. Filme não encontrado na lista ou índice inválido.';
     }
   }
+
+  calcularMedia() {
+    const totalFilmes = this.filmes.length;
+    if (totalFilmes === 0) {
+      this.mediaNotas = 0; // Defina a média como 0 se não houver filmes
+      return;
+    }
+
+    const somaNotas = this.filmes.reduce((acc, filme) => acc + parseFloat(filme.nota || '0'), 0);
+    this.mediaNotas = somaNotas / totalFilmes; // Atualize a propriedade mediaNotas
+  }
+
 }
